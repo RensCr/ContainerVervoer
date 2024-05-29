f7e2a1a3892c672761cc9c9d44d73a31b56caa54
RensCr <rens.crijns@Outlook.com>
2024-05-29 09:10:30 +0200

    Created program

---
+++ ContainerVervoer/Container.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerVervoer
+{
+    public class Container
+    {
+        public int Weigth { get; set; }
+        public bool Valuable { get; set; }
+        public bool Cooled { get; set; }
+    }
+}
---
+++ ContainerVervoer/Ship.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerVervoer
+{
+    public class Ship
+    {
+        private int Length;
+        private int width;
+        private int maxWeigth;
+        public Ship(int length,int width) 
+        { 
+            this.Length = length;
+            this.width = width;
+            this.maxWeigth = CalculateMaxWeight();
+        }
+        private int CalculateMaxWeight()
+        {
+            int maxWeigth = ((Length * width) * 150000);
+            return maxWeigth;
+        }
+    }
+}
